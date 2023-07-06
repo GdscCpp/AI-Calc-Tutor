f3e21952cc7488f8f382f77e58a0a8b580373318
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-07-06 13:33:28 -0700

    files from last commit

--- maxima_server/commands/steps.py
+++ maxima_server/commands/steps.py
@@ -1,13 +1,17 @@
 from sympy.integrals.manualintegrate import *
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.abc import x
-from sympy import integrate, latex
 import sys
 from sympy_gamma.intsteps import *
+from sympy_gamma.diffsteps import *
 
-intg = parse_expr(sys.argv[1])
+mode = sys.argv[1]
+expr = parse_expr(sys.argv[2])
 
-IntegralPrinter(integral_steps(intg,x))
+if mode == "diff":
+    DiffPrinter(diff_steps(expr, x))
+elif mode == "int":
+    IntegralPrinter(integral_steps(expr, x))
 
 
    
