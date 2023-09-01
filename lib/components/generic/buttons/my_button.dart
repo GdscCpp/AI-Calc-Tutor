e5316d49d0f99d94e312cbb8514b419cff23b40b
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-09-01 15:23:56 -0700

    code base refactor

---
+++ lib/components/generic/buttons/my_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+// TODO: rename these to be more relevant to whatever they belong too, ex chat_button
+// no need for button in the name since its already in its own directory
+
+
+class MyButton extends StatelessWidget {
+
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+    
+        ),
+        child: const Center(
+          child: Text(
+            "Sign In",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              ),
+            ),
+        )
+      ),
+    );
+  }
+}
