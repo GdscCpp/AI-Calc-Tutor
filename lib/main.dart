83b86249860a8167f321ea15899d64e06e907980
Bryan Bergo <93395362+blbergo@users.noreply.github.com>
2023-09-04 00:27:26 -0700

    4 refactor code base (#5)
    * added firebase integration, +firebase_helpers
    
    added support for cloud_firestore and firebase_auth
    
    +firebase_helpers - ported from BroncoSafe
    
    * added request format comments
    
    added formatting guidelines for requests in comments for limit, diff and integrate.
    
    * Google Colab for model
    
    * add some support for steps of integration via sympy
    
    python intsteps_mod "func" can now return latex solutions for functions with u sub, power rule, trig, constant and non-repeating integration by parts
    
    * finished updating steps (for now)
    
    * integration with steps added to API
    
    * added support for first derivatives with steps
    
    similar approach to intsteps rework, however much simpler to implement thanks to the custom Rules written for diffPrinter
    
    * files from last commit
    
    * added support for simplification
    
    -plain text or latex expressions can now be simplified using sympy
    
    Note: This is somewhat slow
    
    * Add files via upload
    
    Main chat interface. Still need to add 'X' icon on same row as the 'History' text
    
    * Add files via upload
    
    google and canvas logos used for sign in
    
    * Add files via upload
    
    login page. login with google or canvas
    
    * gradle.build fixes for main.dart
    
    probably errors caused by firebase, works now
    
    * JSON response reformatting
    
    * Add files via upload
    
    * code base refactor
    
    * code base refactor
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Delete firebase_options.dart
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: SididduB807 <59674027+SididduB807@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'repositories/firebase_options.dart';
+
 
 void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +12,7 @@ void main() async  {
   runApp(const MyApp());
 }
 
+// TODO: Rename this
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
